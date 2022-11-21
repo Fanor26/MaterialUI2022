8ccb4707121fc64fc7177973e736c3f3686ac71c
FANOR26 <87057752+Fanor26@users.noreply.github.com>
2022-11-21 06:37:23 +0000

    firts components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Drawer from './components/Drawer';
+
+function App() {
+  return (
+    <div className="App">
+    <Drawer/>
+    </div>
+  );
+}
+
+export default App;
